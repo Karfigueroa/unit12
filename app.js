2bc8510b8936a6ffaffb6226a18d16c90cab4b7d
Figueroa <karfigueroa@FVFZMYC6J1WK.tld>
2022-04-04 10:54:33 -0500

    :)

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 function run(){
-   // document 
+   document.getElementById("paragraph").innerHTML = "hello world!";
+   document.getElementByID("paragraph").style.backgroundColor = "green";
+   document.getElementByID("paragraph").style.color = "white";
+   document.getElementByID("paragraph").style.padding = "20px";
+   randomNum();
+}
+
+function randomNum(){
+   let ran = Math.floor(Math.random() * 10);
+   document.getElementById("random").innerHTML = ran;
 }
